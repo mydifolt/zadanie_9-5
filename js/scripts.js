4c8cf21c41458c7553f692f1a05fdfd71e119714
Piotrek <mydifolt@gmail.com>
2017-05-23 13:27:19 +0200

    1st commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var withButtonClass = document.getElementsByClassName('button');
+var lengthOfButtons = withButtonClass.length;
+function (){
+  for (var i = 0; i < lengthOfButtons; i++);{
+    button = 'innerText';
+    console.log(button);
+}
+  
+}

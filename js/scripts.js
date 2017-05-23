2f4c62790f66d92f7c30f108149da47d7c34076a
Piotrek <mydifolt@gmail.com>
2017-05-23 14:02:47 +0200

    after 1st correction

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,6 @@
 var withButtonClass = document.getElementsByClassName('button');
 var lengthOfButtons = withButtonClass.length;
-function (){
   for (var i = 0; i < lengthOfButtons; i++);{
-    button = 'innerText';
-    console.log(button);
-}
-  
-}
+   	alert(withButtonClass[i]);
+    console.log(btn.innerText);
+   }
